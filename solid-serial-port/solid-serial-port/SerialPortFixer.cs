f447ded2bc8e9bfb56bfb0d2814e8927c6be86b1
ulabs-sgiroux <stefan.giroux@coolcadelectronics.com>
2015-01-12 19:37:18 -0500

    Compilation error

--- solid-serial-port/solid-serial-port/SerialPortFixer.cs
+++ solid-serial-port/solid-serial-port/SerialPortFixer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
